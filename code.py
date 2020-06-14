3723625de35a40d8e4285bcaf77501248f89e0b0
Piyush Sharma <45245975+piyushsharma220699@users.noreply.github.com>
2020-06-14 18:54:08 +0530

    Final Code With Graphics

--- code.py
+++ code.py
@@ -3,12 +3,16 @@ import pygame
 import sys
 import math
 
+ROWLENGTH=6
+COLUMNLENGTH=8
+LENGTHOFBOX=100
+
 def getboard(row,col):
     board=npy.zeros((row,col))
     return board
 
 def valid_location(board,input):
-    return board[5][int(input)] == 0
+    return board[ROWLENGTH-1][int(input)] == 0
 
 def add_piece_to_board(board,input,player):
     row=0
@@ -30,7 +34,7 @@ def check_if_won(board,temprow,tempcol):
         index=index+1
     
     index=1
-    while num<=3 and col+index<=6 and board[row][col]==board[row][col+index]:
+    while num<=3 and col+index<=COLUMNLENGTH-1 and board[row][col]==board[row][col+index]:
         num=num+1
         index=index+1
 
@@ -44,7 +48,7 @@ def check_if_won(board,temprow,tempcol):
         index=index+1
 
     index=1
-    while num<=3 and row+index<=5 and col+index<=6 and board[row][col]==board[row+index][col+index]:
+    while num<=3 and row+index<=ROWLENGTH-1 and col+index<=COLUMNLENGTH-1 and board[row][col]==board[row+index][col+index]:
         num=num+1
         index=index+1
 
@@ -53,12 +57,12 @@ def check_if_won(board,temprow,tempcol):
     
     index=1
     num=1
-    while num<=3 and row+index<=5 and col-index>=0 and board[row][col]==board[row+index][col-index]:
+    while num<=3 and row+index<=ROWLENGTH-1 and col-index>=0 and board[row][col]==board[row+index][col-index]:
         num=num+1
         index=index+1
     
     index=1
-    while num<=3 and row-index>=0 and col+index<=6 and board[row][col]==board[row-index][col+index]:
+    while num<=3 and row-index>=0 and col+index<=COLUMNLENGTH-1 and board[row][col]==board[row-index][col+index]:
         num=num+1
         index=index+1
 
@@ -68,33 +72,33 @@ def check_if_won(board,temprow,tempcol):
     return False
     
 def draw_board(board):
-    for r in range(6):
-        for c in range(7):
-            pygame.draw.rect(screen,(0,0,255),(c*100,(r+1)*100, 100, 100)) 
-            pygame.draw.circle(screen,(0,0,0),(c*100+50,(r+1)*100+50),45)
+    for row in range(ROWLENGTH):
+        for col in range(COLUMNLENGTH):
+            pygame.draw.rect(screen,(0,0,255),(col*LENGTHOFBOX,(row+1)*LENGTHOFBOX, (col+1)*LENGTHOFBOX, (row+2)*LENGTHOFBOX)) 
+            pygame.draw.circle(screen,(0,0,0),(int(col*LENGTHOFBOX+LENGTHOFBOX/2),int((row+1)*LENGTHOFBOX+LENGTHOFBOX/2)),int(LENGTHOFBOX/2-5))
 
-    for r in range(6):
-        for c in range(7):
-            if(board[r][c]==1):
-                pygame.draw.circle(screen,(255,0,0),(c*100+50,700-(r+1)*100+50),45)
-            elif(board[r][c]==2):
-                pygame.draw.circle(screen,(0,255,0),(c*100+50,700-(r+1)*100+50),45)
+    for row in range(ROWLENGTH):
+        for col in range(COLUMNLENGTH):
+            if(board[row][col]==1):
+                pygame.draw.circle(screen,(255,0,0),(int(col*LENGTHOFBOX+LENGTHOFBOX/2),int((ROWLENGTH+1)*LENGTHOFBOX-(row+1)*LENGTHOFBOX+LENGTHOFBOX/2)),int(LENGTHOFBOX/2-5))
+            elif(board[row][col]==2):
+                pygame.draw.circle(screen,(0,255,0),(int(col*LENGTHOFBOX+LENGTHOFBOX/2),int((ROWLENGTH+1)*LENGTHOFBOX-(row+1)*LENGTHOFBOX+LENGTHOFBOX/2)),int(LENGTHOFBOX/2-5))
     
     pygame.display.update()
 
-board = getboard(6,7)
+board = getboard(ROWLENGTH,COLUMNLENGTH)
 someone_won = False
 turn = 1
 
 pygame.init()
-height=700
-width=700
+height=(ROWLENGTH+1)*LENGTHOFBOX
+width=(COLUMNLENGTH)*LENGTHOFBOX
 size=(width,height)
 screen=pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
 
-font=pygame.font.SysFont("comicsansms", 80)
+font=pygame.font.SysFont("comicsansms", 70)
 
 while not someone_won:
 
@@ -103,39 +107,39 @@ while not someone_won:
             sys.exit
         
         if event.type==pygame.MOUSEMOTION:
-            pygame.draw.rect(screen,(0,0,0),(0,0,700,100))
+            pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
             position=event.pos[0]
             if turn == 1:
-                pygame.draw.circle(screen,(255,0,0),(position,50),50)
+                pygame.draw.circle(screen,(255,0,0),(position,int(LENGTHOFBOX/2)),int(LENGTHOFBOX/2))
             elif turn == 2:
-                pygame.draw.circle(screen,(0,255,0),(position,50),50)
+                pygame.draw.circle(screen,(0,255,0),(position,int(LENGTHOFBOX/2)),int(LENGTHOFBOX/2))
         
         pygame.display.update()
 
         if event.type==pygame.MOUSEBUTTONDOWN:
             if turn == 1:
                 position=event.pos[0]
-                user_input=int(math.floor(position/100))
+                user_input=int(math.floor(position/LENGTHOFBOX))
 
                 if valid_location(board,user_input):
                     row=add_piece_to_board(board,user_input,1)
                     if check_if_won(board,row,user_input):
-                        pygame.draw.rect(screen,(0,0,0),(0,0,700,100))
+                        pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
                         label=font.render("PLAYER 1 WINS",1,(255,0,0))
-                        screen.blit(label, (30,10))
+                        screen.blit(label, (40,0))
                         someone_won=True
 
 
             else:
                 position=event.pos[0]
-                user_input=int(math.floor(position/100))
+                user_input=int(math.floor(position/LENGTHOFBOX))
 
                 if valid_location(board,user_input):
                     row=add_piece_to_board(board,user_input,2)
                     if check_if_won(board,row,user_input):
-                        pygame.draw.rect(screen,(0,0,0),(0,0,700,100))
+                        pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
                         label=font.render("PLAYER 2 WINS",1,(0,255,0))
-                        screen.blit(label, (30,10))
+                        screen.blit(label, (40,0))
                         someone_won=True
 
             draw_board(board)
