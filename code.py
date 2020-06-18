0ea52129d3abb6d7bbc1863c360b30260828797e
Piyush Sharma <45245975+piyushsharma220699@users.noreply.github.com>
2020-06-18 23:32:31 +0530

    Poor AI Support added to Player 1

--- code.py
+++ code.py
@@ -7,6 +7,7 @@ import math
 from pygame import mixer
 from sys import exit
 from pygame.locals import *
+import random
 
 ROWLENGTH=6
 COLUMNLENGTH=8
@@ -68,6 +69,9 @@ def introduction(startgame):
 
             if event.type==MOUSEBUTTONDOWN:
                 mouse=pygame.mouse.get_pos()
+                if 200+400>mouse[0]>200 and 150+100>mouse[1]>150:
+                    looponeplayer()
+                    runit=False
                 if 200+400>mouse[0]>200 and 350+100>mouse[1]>350:
                     startgame=True
                     runit=False
@@ -224,6 +228,146 @@ def loopit(height,width,endgame):
                     mixer.music.play()
                     pygame.time.wait(5000)
 
+
+def looponeplayer():
+    board = getboard(ROWLENGTH,COLUMNLENGTH)
+    draw_board(board)
+    pygame.display.update()
+    mixer.music.load('startsound.ogg')
+    mixer.music.play()
+    someone_won=False
+    turn=1
+    while not someone_won:
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                endgame=True
+                return endgame
+            
+            if event.type==pygame.MOUSEMOTION:
+                pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
+                position=event.pos[0]
+                if turn == 1:
+                    pygame.draw.circle(screen,(255,0,0),(position,int(LENGTHOFBOX/2)),int(LENGTHOFBOX/2))
+                elif turn == 2:
+                    pygame.draw.circle(screen,(0,255,0),(position,int(LENGTHOFBOX/2)),int(LENGTHOFBOX/2))
+            
+            pygame.display.update()
+
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if turn == 1:
+                    position=event.pos[0]
+                    user_input=int(math.floor(position/LENGTHOFBOX))
+
+                    if valid_location(board,user_input):
+                        row=add_piece_to_board(board,user_input,1)
+                        mixer.music.load('insertsound.wav')
+                        mixer.music.play()
+                        if check_if_won(board,row,user_input):
+                            pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
+                            label=font.render("PLAYER 1 WINS!!!",1,(255,0,0))
+                            text_rect=label.get_rect(center=(int((COLUMNLENGTH*LENGTHOFBOX)/2), int(LENGTHOFBOX/2)))
+                            screen.blit(label, text_rect)
+                            someone_won=True
+                        
+                    if turn == 1:
+                        turn=2
+                    else:
+                        turn=1
+                    draw_board(board)
+                    pygame.display.update()
+
+
+        if turn==2 and not someone_won:
+            pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
+            pygame.display.update()
+            col=random.randint(0,COLUMNLENGTH-1)
+            if valid_location(board,col):
+                pygame.time.wait(1000)
+                row=add_piece_to_board(board,col,2)
+                mixer.music.load('insertsound.wav')
+                mixer.music.play()
+                if check_if_won(board,row,col):
+                    pygame.draw.rect(screen,(0,0,0),(0,0,COLUMNLENGTH*LENGTHOFBOX,LENGTHOFBOX))
+                    label=font.render("COMPUTER WINS!!!",1,(0,255,0))
+                    text_rect=label.get_rect(center=(int((COLUMNLENGTH*LENGTHOFBOX)/2), int(LENGTHOFBOX/2)))
+                    screen.blit(label, text_rect)
+                    someone_won=True
+
+                draw_board(board)        
+                if turn == 1:
+                    turn=2
+                else:
+                    turn=1
+        
+        if someone_won:
+            mixer.music.load('winsound.mp3')
+            mixer.music.play()
+            pygame.time.wait(5000)
+        
+    restartgamewithAI()
+
+
+
+# def score_position(board,piece):
+#     score=0
+#     for row in range(ROWLENGTH):
+#         row_array=[int(i) for i in list(board[row,:])]
+#         for col in range(COLUMNLENGTH-3):
+#             window=row_array[col:col+WINDOW_LENGTH]
+
+#             if window.count(piece)==4:
+#                 score+=100
+#             elif window.count(piece)==3 and window.count(EMPTY)==1:
+#                 score+=10
+    
+#     for col in range(COLUMNLENGTH):
+#         col_array=[int(i) for i in list(board[col,:])]
+#         for col in range(ROWLENGTH-3):
+#             window=col_array[row:row+WINDOW_LENGTH]
+
+#             if window.count(piece)==4:
+#                 score+=100
+#             elif window.count(piece)==3 and window.count(EMPTY)==1:
+#                 score+=10
+    
+#     return score
+
+# def alphabetapruning(board,depth,alpha,beta,maximizingplayer)
+# {
+#     if 
+
+#     if maximizingplayer:
+#         value= -math.inf
+#         for i=0 to board[0].size():
+#             if valid_location(board,i):
+#                 board_copy=board.copy()
+#                 add_piece_to_board(board,i,AI)
+#                 new_score=alphabetapruning(board_copy,depth-1,alpha,beta,False)[1]
+#                 if new_score>value:
+#                     value=new_score
+#                     column=col
+#                 alpha=max(alpha,value)
+#                 if alpha>=beta:
+#                     break    
+#         return column,value
+    
+#     else:
+#         value=math.inf
+#         for i=0 to board[0].size():
+#             if valid_loaction(board,i):
+#                 board_copy=board.copy()
+#                 add_piece_to_board(board,i,PLAYER)
+#                 new_score=alphabetapruning(board_copy,depth-1,alpha,beta,True)[1]
+#                 if new_score<value:
+#                     value=new_score
+#                     column=col
+#                 beta=min(beta,value)
+#                 if alpha>=beta:
+#                     break    
+#         return column,value
+# }
+
 def restartgame(restart):
     for row in range(ROWLENGTH):
         for col in range(COLUMNLENGTH):
@@ -264,6 +408,48 @@ def restartgame(restart):
         pygame.display.update()
                     
     return restart
+
+
+def restartgamewithAI():
+    for row in range(ROWLENGTH):
+        for col in range(COLUMNLENGTH):
+            pygame.draw.rect(screen,(0,0,0),(col*LENGTHOFBOX,row*LENGTHOFBOX, (col+1)*LENGTHOFBOX, (row+1)*LENGTHOFBOX))
+    
+    label=font.render("PLAY AGAIN?",1,(0,0,255))
+    text_rect=label.get_rect(center=(int((COLUMNLENGTH*LENGTHOFBOX)/2), int(LENGTHOFBOX/2)))
+    screen.blit(label, text_rect)
+    pygame.display.update()
+    runit=True
+    while runit:
+        pygame.draw.rect(screen,(0,255,0),(200,200,400,100))
+        pygame.draw.rect(screen,(255,0,0),(200,400,400,100))
+        mouse=pygame.mouse.get_pos()
+        if 200+400>mouse[0]>200 and 200+100>mouse[1]>200:
+            pygame.draw.rect(screen,(0,255,220),(200,200,400,100))
+        if 200+400>mouse[0]>200 and 400+100>mouse[1]>400:
+            pygame.draw.rect(screen,(255,0,220),(200,400,400,100))
+        label=smallfont.render("RESTART",1,(0,0,0))
+        text_rect=label.get_rect(center=(400,250))
+        screen.blit(label, text_rect)
+        label2=smallfont.render("QUIT",1,(0,0,0))
+        text_rect2=label2.get_rect(center=(400,450))
+        screen.blit(label2, text_rect2)        
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                runit=False
+
+            if event.type==MOUSEBUTTONDOWN:
+                mouse=pygame.mouse.get_pos()
+                if 200+400>mouse[0]>200 and 200+100>mouse[1]>200:
+                    looponeplayer()
+                    runit=False
+                if 200+400>mouse[0]>200 and 400+100>mouse[1]>400:
+                    runit=False
+
+        pygame.display.update()
+
+
 
 startgame=introduction(False)
 
